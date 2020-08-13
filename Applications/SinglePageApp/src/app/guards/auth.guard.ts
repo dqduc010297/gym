7a86147a9423a0bff2ec0bf9f6add7ee4367d66a
d.doan <quang.duc.doan@steadfasttech.com.au>
2020-08-13 16:49:05 +0700

    deploy to firebase

--- Applications/SinglePageApp/src/app/guards/auth.guard.ts
+++ Applications/SinglePageApp/src/app/guards/auth.guard.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
     constructor(private authService: AuthService, private router: Router) { }
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        return true;
         const currentUser = this.authService.currentUserValue;
         if (currentUser) {
             return true;
