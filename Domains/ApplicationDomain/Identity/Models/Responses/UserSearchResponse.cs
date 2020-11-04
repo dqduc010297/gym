a144cb114c622036be9289e041eb94424c67ee3b
ducd <Quang.Duc.Doan@steadfasttech.com.au>
2020-11-04 08:22:33 +0700

    Replace fullname and phonenumber

--- Domains/ApplicationDomain/Identity/Models/Responses/UserSearchResponse.cs
+++ Domains/ApplicationDomain/Identity/Models/Responses/UserSearchResponse.cs
@@ -7,8 +7,7 @@ namespace ApplicationDomain.Identity.Models.Responses
     public class UserSearchResponse
     {
         public int Id { set; get; }
-        public string Fullname { set; get; }
+        public string Label { set; get; }
         public string AvatarURL { set; get; }
-        public string PhoneNumber { set; get; }
     }
 }
