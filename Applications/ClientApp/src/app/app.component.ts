efc53df2cd503749e9c9ee325510d65d3eaab3f7
unknown <Administrator@WIN-TCRAF9J851N.rufademodns.com>
2020-11-04 08:26:08 +0700

    Check version

--- Applications/ClientApp/src/app/app.component.ts
+++ Applications/ClientApp/src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { AuthService } from './auth/core/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    private authService: AuthService
+  ) {
+
+  }
+  ngOnInit(): void {
+    // get version storage
+    const appVersion = localStorage.getItem('appVersion');
+    if (appVersion) {
+      if (appVersion === environment.appVersion) {
+        return;
+      }
+    }
+    this.authService.logout();
+  }
 }
